fc1f6687b8a12308067871d063eb8146cf2c7915
jawlee <41410091+jawlee@users.noreply.github.com>
2021-03-26 19:26:51 -0400

    Add files via upload
    Code Refactoring. Defined Methods. 
    Now checks for add to cart button as stock indicator
    Checks to make sure it is fulfilled by Amazon to ensure it's not a 3rd party vendor
    Added Performance Timers

--- webs_amazon.py
+++ webs_amazon.py
@@ -7,57 +7,85 @@ import requests
 
 from datetime import datetime
 
-date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+
+import time
 
 from discord import Webhook, RequestsWebhookAdapter
-# Set up Discord
-discordwebhook = Webhook.from_url("https://discord.com/api/webhooks/824516450730901515/fCwDFCAQhqFgsErMgEh9rt8LKF-KzrgX-BIABDCHMfDv2YiDW2Aq3RUEe4hTFo7icGWC", adapter=RequestsWebhookAdapter())
-
-#Set up Chrome
-driver = webdriver.Chrome(executable_path=binary_path)
 
 urlList = [
     "https://www.amazon.ca/Dogs-Sofa-Jigsaw-Puzzle-Piece/dp/B07S9MP986/",
     "https://www.amazon.ca/MSI-GeForce-RTX-3070-Architecture/dp/B08KWPDXJZ/",
     "https://www.amazon.ca/EVGA-GeForce-3060-Graphics-08G-P5-3663-KR/dp/B08R876RTH/",
-    "https://www.amazon.ca/EVGA-10G-P5-3897-KR-GeForce-Technology-Backplate/dp/B08HR3Y5GQ",
-    "https://www.amazon.ca/Graphics-IceStorm-Advanced-Lighting-ZT-A30700H-10P/dp/B08LF1CWT2",
-    "https://www.amazon.ca/EVGA-10G-P5-3897-KR-GeForce-Technology-Backplate/dp/B08HR3Y5GQ/"
+    # "https://www.amazon.ca/EVGA-10G-P5-3897-KR-GeForce-Technology-Backplate/dp/B08HR3Y5GQ",
+    # "https://www.amazon.ca/Graphics-IceStorm-Advanced-Lighting-ZT-A30700H-10P/dp/B08LF1CWT2",
+    # "https://www.amazon.ca/EVGA-10G-P5-3897-KR-GeForce-Technology-Backplate/dp/B08HR3Y5GQ/"
     ]
-print ("Done Setup\n\n\n")
 
-for url in urlList:
+def setup():
+    startSetupTime = time.perf_counter()
+
+    # Set up Date
+    global date 
+    date = datetime.now().strftime("%Y_%m_%d-%I:%M:%S_%p")
+    
+    # Set up Discord
+    global discordwebhook 
+    discordwebhook = Webhook.from_url("https://discord.com/api/webhooks/824516450730901515/fCwDFCAQhqFgsErMgEh9rt8LKF-KzrgX-BIABDCHMfDv2YiDW2Aq3RUEe4hTFo7icGWC", adapter=RequestsWebhookAdapter())
+
+    #Set up Chrome
+    global driver 
+    driver = webdriver.Chrome(executable_path=binary_path)
+
+    
+    endSetupTime = time.perf_counter()
+    dur = endSetupTime - startSetupTime
+    print (f"Done Setup in {dur} seconds\n\n\n")
+
+def checkStock(url):
     driver.get(url)
 
     content = driver.page_source
     soup = BeautifulSoup(content, 'html.parser')
-    availMsg=soup.find('span', attrs={'class':'a-color-success'})
-    print("availMsg is: ")
-    print(availMsg)
+    availMsg = soup.find('input', attrs={'id':'add-to-cart-button'})
+    merchantInfo = soup.find('a', attrs={'id':'SSOFpopoverLink'})
+    # print("availMsg is: ")
+    # print(availMsg)
 
-    if "Coming soon" in availMsg:
-        stockStatus='COMING SOON'
-    elif "In Stock" in availMsg:
-        driver.find_element_by_id("add-to-cart-button").click()
-        stockStatus='IN STOCK!!!!!!!!!!!!!'
-        discordwebhook.send('@gpu-stock AMAZON STOCK ALERT!\n' +url)
+    stockStatus ="Not defined yet"
 
-        #Save HTML to file
-            availFile_Amazon = open("Available_AMZ_"+date+".txt","w+")
-            availFile_Amazon.write(url+"\n\n")
-            availFile_Amazon.write(soup)
-            availFile_Amazon.close()
+    if availMsg is not None and "Fulfilled by Amazon" in merchantInfo:
+        try:
+            driver.find_element_by_id("add-to-cart-button").click()
+
+            stockStatus='IN STOCK!!!!!!!!!!!!!'
+            # discordwebhook.send('@gpu-stock TESTING!!!! AMAZON STOCK ALERT!\n' +url)
+
+            #Save HTML to file
+            # availFile_Amazon = open(f'Available_AMZ_{date}.txt',"w+")
+            # availFile_Amazon.write(url+"\n\n")
+            # availFile_Amazon.write(str(soup))
+            # availFile_Amazon.close()
+            # driver.find_element_by_id("add-to-cart-button").click()
+        except:
+            pass
     else:
-        stockStatus ="elsenothing"
-        print('something else')
-        driver.find_element_by_id("add-to-cart-button").click()
-    print ("===========================================================================\n")
+        stockStatus ="Not Available / Not Fulfilled by Amazon"
+        # print(stockStatus)
+        
+    print("===========================================================================\n")
     print(url)
     print('Availability is: ' +stockStatus)
-    print(availMsg)
-    print ("===========================================================================\n")
 
-# print ("html file")
-#print(soup)
+def main():
+    setup()
+    print(urlList)
+    for u in urlList:
+        startCheckTime = time.perf_counter()
+        checkStock(u)
+        endCheckTime = time.perf_counter()
+        durCheck = endCheckTime - startCheckTime
+        print(f"Duration: {durCheck} seconds")
 
+if __name__ == "__main__":
+    main()
 
